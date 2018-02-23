554a5c4e7d6002752ec8e9890b2363a8da499524
ClawDeck <clawdeck9@gmail.com>
2018-02-23 15:33:49 +0100

    security is ok, a rest servie to add accounts has been added too. The jwt is still not implemented here

--- src/main/java/com/cluster9/jwtspringsecurity/entities/AppUser.java
+++ src/main/java/com/cluster9/jwtspringsecurity/entities/AppUser.java
@@ -10,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
