854dcd3d12e3532873c613e6ae3a3614bed79ba0
ClawDeck <clawdeck9@gmail.com>
2018-02-23 17:24:59 +0100

    security is ok, a rest servie to add accounts has been added too. The jwt is still not implemented here

--- src/main/java/com/cluster9/jwtspringsecurity/web/AccountRestController.java
+++ src/main/java/com/cluster9/jwtspringsecurity/web/AccountRestController.java
@@ -1,5 +1,7 @@
 package com.cluster9.jwtspringsecurity.web;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,16 @@ public class AccountRestController {
 	@Autowired
 	private AccountService ac;
 	@RequestMapping("/register")
-	public AppUser register(@RequestBody AppUser user){
-		return ac.saveUser(user);
+	public AppUser register(@RequestBody RegisterForm  userForm){
+		if(ac.findUserByUsername(userForm.getUsername()) != null) 
+			throw new RuntimeException("The user already exists");
+		if(!(userForm.getPassword().equals(userForm.getRepassword())))
+			throw new RuntimeException("the passwords are different");
+		AppUser user = new AppUser();
+		user.setUsername(userForm.getUsername());
+		user.setPassword(userForm.getPassword());
+		ac.saveUser(user);
+		ac.addRoleToUser(user.getUsername(), "USER");
+		return user;
 	}
 }
